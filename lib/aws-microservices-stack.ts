5680adb7166dede1efafbd871a20d05bbdc62ac4
lakshithadev <lxkshitha@gmail.com>
2023-03-28 09:13:16 +0530

    create custom solution contructs

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,71 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-import { join } from 'path';
+import { SwnApiGateway } from './apigateway';
+import { SwnDatabase } from './database';
+import { SwnMicroservices } from './microservice';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const productTable = new Table(this, 'product', {
-      partitionKey: {
-        name: 'id',
-        type: AttributeType.STRING
-      },
-      tableName: 'product',
-      removalPolicy: RemovalPolicy.DESTROY,
-      billingMode: BillingMode.PAY_PER_REQUEST
+    const database = new SwnDatabase(this, 'Database')
+
+    const microservices = new SwnMicroservices(this, 'Microservices', {
+      productTable: database.productTable
     })
 
-    const nodejsFunctionProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: [
-          'aws-sdk'
-        ]
-      },
-      environment: {
-        PRIMARY_KEY: 'id',
-        DYNAMODB_TABLE_NAME: productTable.tableName
-      },
-      runtime: Runtime.NODEJS_18_X
-    }
-
-    const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
-      entry: join(__dirname, `/../src/product/index.js`),
-      ...nodejsFunctionProps
+    const apigateway = new SwnApiGateway(this, 'ApiGateway', {
+      productMicroservice: microservices.productMicroservice
     })
-
-    productTable.grantReadWriteData(productFunction)
-
-    // Product micrservices API GATEWAY
-    // root name = product
-    // GET /product
-    // POST /product
-    
-    // Single product with id parameter
-    // GET /product/{id}
-    // PUT /product/{id}
-    // DELETE /product/{id}
-
-    const apigw = new LambdaRestApi(this, 'productApi', {
-      restApiName: 'Product Service',
-      handler: productFunction,
-      proxy: false
-    })
-
-    const product = apigw.root.addResource('product')
-    product.addMethod('GET')
-    product.addMethod('POST')
-
-    const singleProduct = product.addResource('{id}')
-    singleProduct.addMethod('GET')
-    singleProduct.addMethod('PUT')
-    singleProduct.addMethod('DELETE')
 
   }
 }
